0d38fd39ebe9802ab21f578a34c70cb3738bdbad
Ben Schriner <schriner05@gmail.com>
2023-04-04 13:32:30 -0400

    are we done yet?

--- frontend/src/orders/data.tsx
+++ frontend/src/orders/data.tsx
@@ -1,11 +1,39 @@
 export default {
     orders: [{
         _id: 1,
-        createdAt: '02/20/2'
-    }]
-}export default {
-    orders: [{
+        createdAt: '04/04/2023',
+        totalPrice: 42.90,
+        user: {
+            name: 'Ian'
+        },
+        isPaid: true,
+        paidAt: '04/01/2023',
+        isDelivered: false,
+        deliveredDate: null,
+    },
+    {
         _id: 1,
-        createdAt: '02'
-    }]
+        createdAt: '04/04/2023',
+        totalPrice: 42.90,
+        user: {
+            name: 'Ben'
+        },
+        isPaid: true,
+        paidAt: '04/01/2023',
+        isDelivered: false,
+        deliveredDate: null,
+    },
+    {
+        _id: 1,
+        createdAt: '04/04/2023',
+        totalPrice: 42.90,
+        user: {
+            name: 'Charles'
+        },
+        isPaid: true,
+        paidAt: '04/01/2023',
+        isDelivered: false,
+        deliveredDate: null,
+    },
+]
 }
